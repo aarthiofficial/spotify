c0402d37541246690c22711fce988547f7dff1dc
aarthiofficial <aarthi.nbs@gmail.com>
2025-05-29 19:59:36 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import './globals.css'
+import { Inter } from 'next/font/google'
+import { SessionProvider } from 'next-auth/react'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Simplified Spotify Homepage Clone',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>{children}</SessionProvider>
+      </body>
+    </html>
+  )
+}
